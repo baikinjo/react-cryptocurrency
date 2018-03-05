ccc02983991f77085da3c0e7db6a821c185a7573
Injo Baik <baikinjo.28@gmail.com>
2018-03-05 01:27:29 -0800

    about.js

--- src/components/About.js
+++ src/components/About.js
@@ -8,12 +8,12 @@ const About = () => {
             <p>Tools used:</p>
 
             <ul>
-                <li>Framework: <a href="https://reactjs.org/">React.js</a></li>
-                <li>Styling: <a href="http://materializecss.com/">Materialize-CSS</a></li>
-                <li>API: <a href="https://coinmarketcap.com/api/">Cryptocurrency Market Capitalizations</a></li>
-                <li>Coin icon image: <a href="https://github.com/cjdowner/cryptocurrency-icons">cjdowner's repo</a></li>
-                <li>Deployment: <a href="http://reactcrypto.injobaik.ca/">Heroku</a></li>
-                <li>Source Code: <a href="https://github.com/baikinjo/react-cryptocurrency">GitHub</a></li>
+                <li>Framework: <a target="_blank" href="https://reactjs.org/">React.js</a></li>
+                <li>Styling: <a target="_blank" href="http://materializecss.com/">Materialize-CSS</a></li>
+                <li>API: <a target="_blank" href="https://coinmarketcap.com/api/">Cryptocurrency Market Capitalizations</a></li>
+                <li>Coin icon image: <a target="_blank" href="https://github.com/cjdowner/cryptocurrency-icons">cjdowner's repo</a></li>
+                <li>Deployment: <a target="_blank" href="http://reactcrypto.injobaik.ca/">Heroku</a></li>
+                <li>Source Code: <a target="_blank" href="https://github.com/baikinjo/react-cryptocurrency">GitHub</a></li>
             </ul>
 
         </div>
